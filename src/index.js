f5ff0e84be9539fb2bbaa9e045e44e80dd2700e8
Anastasia <novakanastasya@yandex.ru>
2018-02-27 23:36:18 +0300

    Implementation of getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  let dividers = {};
+
+  let i = 2;
+  while (i <= base) {
+    if (base % i === 0) {
+      if (dividers[i] === undefined) {
+        dividers[i] = 0;
+      }
+      dividers[i] += 1;
+      base = Math.round(base / i);
+    } else {
+      i += 1;
+    }
+  }
+
+  const solutions = Object.keys(dividers).map((d) => {
+    let r = 0;
+    let n = number;
+    while(n >= 1) {
+      n = Math.floor(n / d);
+      r += n;
+    }
+    return Math.floor(r / dividers[d]);
+  });
+
+  return Math.min(...solutions);
+};
